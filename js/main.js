42246fe7714dce9b54b4b0f5b81e4aa23860a8dc
potatokyun <morandartedj123@gmail.com>
2026-09-16 00:40:15 +0800

    Modal functionality 1.  SEarch using API 2. Display locations based on text searched 3. Selectable location to change location for main 4. structure for ui and data for request to main.

--- js/main.js
+++ js/main.js
@@ -4,15 +4,17 @@ import { updateCurrentTime } from "./uiBuilder.js"
 import { locationDataBuilder } from "./locationDataBuilder.js"
 
 //Main function
-async function main(){
-    let geolocation = await getLatLong();                                           //waits the Promise resolves or rejects
+async function main(geolocation){
+
+    if(!geolocation){
+        geolocation = await getLatLong();                                        //waits the Promise resolves or rejects
+    }
+
     let weatherapidata = await getForecast(geolocation.latitude, geolocation.longitude); 
     let locationapidata = await getLocationData(geolocation.latitude, geolocation.longitude); //waits the Promise resolves or rejects
     let weatherdata = weatherDataBuilder(weatherapidata.current, weatherapidata.current_units);
     let locationdata = locationDataBuilder(locationapidata);
     uiSetter(weatherdata, locationdata);
-    console.log(locationdata);
-    console.log(weatherdata);
 }
 
 //Get Latitude and Longitude from Browser/Client
@@ -67,7 +69,7 @@ async function getForecast(latitude, longitude){
     return resdata;
 }
 
-//Request to the API BIG DATAC LOUD
+//Request to the API BIG DATA CLOUD
 async function getLocationData(latitude, longitude){
     
     let strrequest = await `https://api-bdc.net/data/reverse-geocode?`+
@@ -80,9 +82,121 @@ async function getLocationData(latitude, longitude){
     return resdata;
 }
 
+//Request Geo LOc based on text
+async function searchLocation(){
+    let searchtext = document.getElementById('search-input').value;
+
+    let strrequest = await `https://geocoding-api.open-meteo.com/v1/search?name=${searchtext}&count=10&language=en&format=json`;
+    
+    let res = await fetch(strrequest);
+    let resdata = await res.json();
+    return resdata.results;
+
+}
+
+function buildSuggestions(listResult){
+    let locsuggestdiv = document.getElementById("loc-suggestion");
+    locsuggestdiv.style.color = 'black';
+    locsuggestdiv.innerHTML = '';
+
+    listResult.map((value, index)=>{
+        let resdiv = document.createElement('div');
+        resdiv.className = "col-12 search-selectables";
+      
+        let locationtext = `${index+1}.` + locationBuilder(value);
+    
+        resdiv.textContent = locationtext;
+        resdiv.id = index;
+        resdiv.onclick = selectLocation;
+
+        locsuggestdiv.appendChild(resdiv);
+    })
+}
+
+function locationBuilder(locdata){
+    let descarray = [];
+    let loctext = '';
+    descarray.push(locdata.name);
+
+    if(locdata.admin3){
+        (descarray[descarray.length-1] == locdata.admin3 ) ? null : descarray.push(locdata.admin3);
+    }
+    if(locdata.admin2){
+        (descarray[descarray.length-1] == locdata.admin2 ) ? null : descarray.push(locdata.admin2);
+    }
+    if(locdata.admin1){
+        (descarray[descarray.length-1] == locdata.admin1 ) ? null : descarray.push(locdata.admin1);
+    }
+    if(locdata.country){
+        (descarray[descarray.length-1] == locdata.country ) ? null : descarray.push(locdata.country);
+    }
+
+    descarray.map((value, index) => {
+        if(index == 0){
+            loctext += value;
+        }else{
+            loctext += ", " + value;
+        }
+    });
+
+    return loctext;
+}
+
+function selectLocation(){
+    let locIndex = this.id;
+    let locationData = locations[locIndex];
+
+    main({latitude:locationData.latitude, longitude:locationData.longitude});
+    closemodal();
+}
+
+function clearmodalsuggestion(){
+    let locsuggestdiv = document.getElementById("loc-suggestion");
+    let searchinput = document.getElementById("search-input");
+    locsuggestdiv.style.color = 'black';
+    locsuggestdiv.innerHTML = '';
+    searchinput.value = '';
+}
+
+function closemodal(){
+    document.getElementById('myModal').style.display = 'none';
+    clearmodalsuggestion();
+}
+
 
 
 //Initial
 updateCurrentTime();
 setInterval(updateCurrentTime, 1000);
-main();
+main(false);
+let locations = [];
+
+
+//Modal functionality
+document.getElementById('search-location').addEventListener('click', () => {
+    document.getElementById('myModal').style.display = 'flex';
+    clearmodalsuggestion();
+});
+document.getElementById('modal-close').addEventListener('click', () => {
+    closemodal();
+});
+//Search Button
+document.getElementById('search-button').addEventListener('click', async () => {
+    let searchLocData = await searchLocation();
+    let locsuggestdiv = document.getElementById("loc-suggestion");
+    if(searchLocData){
+        if(searchLocData.length >= 1){
+            locations = searchLocData;
+            buildSuggestions(searchLocData);
+        }else{
+            locsuggestdiv.textContent = 'NO LOCATION FOUND!';
+            locsuggestdiv.style.color = "red";
+        }
+    }else{
+        locsuggestdiv.textContent = 'NO LOCATION FOUND!';
+        locsuggestdiv.style.color = "red";
+    }
+});
+
+
+// setInterval(main, 20 * 60 * 1000); // 20 minutes, matching your earlier plan
